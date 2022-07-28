7e53b11c04a3c9eb657ebeaecc7f521c478d3bd5
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-28 13:59:18 +0200

    function types: () => number fe.

--- 02 - TypeScript Basics & Basic Types/course-starting-project/app.ts
+++ 02 - TypeScript Basics & Basic Types/course-starting-project/app.ts
@@ -2,6 +2,7 @@ function add(n1: number, n2: number) {
   return n1 + n2
 }
 
+// "void" for function which dont return a value
 function printResult(num: number): void {
   console.log('Result: ' + num)
 }
@@ -9,3 +10,11 @@ function printResult(num: number): void {
 // let someValue: undefined
 
 printResult(add(5, 12))
+
+let combineValues: (a: number, b: number) => number
+// combineValues = 5
+
+combineValues = add
+// combineValues = printResult
+
+console.log(combineValues(8, 8))
