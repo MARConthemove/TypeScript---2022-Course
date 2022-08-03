a74420fec27672a723d2ae9fbc93438ac931fdd9
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-08-03 11:21:20 +0200

    arrow functions with tsc

--- 04 - Next-generation JavaScript & TypeScript/next-gen-01-starting-setup/src/app.ts
+++ 04 - Next-generation JavaScript & TypeScript/next-gen-01-starting-setup/src/app.ts
@@ -4,16 +4,33 @@ let age = 30
 
 age = 29
 
-function add(a: number, b: number) {
-  var result
-  result = a + b
-  return result
+// function add(a: number, b: number) {
+//   var result
+//   result = a + b
+//   return result
+// }
+
+const add = (a: number, b: number) => a + b
+
+const printOutput: (a: string | number) => void = output  => {
+  console.log(output)
 }
+
+const button = document.querySelector("button")
+
+if (button) {
+  button.addEventListener("click", event => console.log(event))
+}
+
+console.log(add(2,5))
+
+printOutput(add(5,2))
 
 // with let active blockscope!
-if (age > 20) {
-  let isOld = true
-}
+// if (age > 20) {
+//   let isOld = true
+// }
 
-console.log(isOld)
+// console.log(isOld)
+
 // console.log(result)
