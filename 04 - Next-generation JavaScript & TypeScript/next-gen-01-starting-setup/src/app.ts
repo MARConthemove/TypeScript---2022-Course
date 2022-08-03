7239c33d5fdaa6f092916bd24c4434c106af56f4
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-08-03 11:34:15 +0200

    rest parameters in tsc => actively declaring data types needed

--- 04 - Next-generation JavaScript & TypeScript/next-gen-01-starting-setup/src/app.ts
+++ 04 - Next-generation JavaScript & TypeScript/next-gen-01-starting-setup/src/app.ts
@@ -12,26 +12,26 @@ age = 29
 
 // default parameter for "b"
 // default arguments have to be at the last position otherwise it will not work!
-const add = (a: number, b: number = 1) => a + b
+// const add = (a: number, b: number = 1) => a + b
 
-const printOutput: (a: string | number) => void = output  => {
-  console.log(output)
-}
+// const printOutput: (a: string | number) => void = output  => {
+//   console.log(output)
+// }
 
-const button = document.querySelector("button")
+// const button = document.querySelector("button")
 
-if (button) {
-  button.addEventListener("click", event => console.log(event))
-}
+// if (button) {
+//   button.addEventListener("click", event => console.log(event))
+// }
 
-const hobbies = ["sports", "cooking"]
+// const hobbies = ["sports", "cooking"]
 
 // spread opperator:
-const activeHobbies = ["hiking", ...hobbies]
+// const activeHobbies = ["hiking", ...hobbies]
 
 // activeHobbies.push(hobbies[0], hobbies[1])
 // better:
-activeHobbies.push(...hobbies)
+// activeHobbies.push(...hobbies)
 
 const person = {
   name: "Max",
@@ -40,11 +40,20 @@ const person = {
 
 const copiedPerson = { ...person }
 
-console.log(copiedPerson)
-console.log(activeHobbies)
-console.log(add(2,5))
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue
+  }, 0)
+}
 
-printOutput(add(5))
+const addedNumbers = add(5, 10 , 2, 3.7)
+console.log(addedNumbers)
+
+console.log(copiedPerson)
+// console.log(activeHobbies)
+// console.log(add(2,5))
+
+// printOutput(add(5))
 
 // with let active blockscope!
 // if (age > 20) {
