97db82f0b19b8f81cecf9503372f568d3333f388
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-08-09 11:05:29 +0200

    private and public access modifiers in tsc with class

--- 05 - Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05 - Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,22 +1,44 @@
 class Department {
-  name: string = "DEFAULT"
+  // public is default = accessable from outside
+  // private only allows to change properties with methods from inside the class
+  public name: string = "DEFAULT"
+  private employees: string[] = []
 
   // function which is executed when an instance is getting created
   constructor(n: string) {
     this.name = n
   }
 
+  // methods
   describe(this: Department) {
     console.log('Department: ' + this.name)
   }
+  addEmployee(employee: string) {
+    // validation etc
+    this.employees.push(employee)
+  }
+  printEmloyeeInformation() {
+    console.log("length: ", this.employees.length)
+    console.log("employee array: ", this.employees)
+  }
+
 }
 
 const accounting = new Department("Accounting")
 
-const accountingCopy = { name: "M.Laundry", describe: accounting.describe }
+accounting.addEmployee("Max")
+accounting.addEmployee("Manu")
 
+// this shouldn't be allowed!
+// accounting.employees[2] = "Anna"
+
+accounting.printEmloyeeInformation()
 
 accounting.describe()
-accountingCopy.describe()
 
-console.log(accounting)
+
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe }
+
+// accountingCopy.describe()
+
+
