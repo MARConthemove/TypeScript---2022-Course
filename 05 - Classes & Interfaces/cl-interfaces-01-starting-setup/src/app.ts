5bf1eefbe836465cc3a15fd0ff739033831b32ad
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-08-09 10:47:08 +0200

    compiling with different js versions in tsc

--- 05 - Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05 - Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+class Department {
+  name: string = "DEFAULT"
+
+  // function which is executed when an instance is getting created
+  constructor(n: string) {
+    this.name = n
+  }
+}
+
+const accounting = new Department("Accounting")
+
+console.log(accounting)
