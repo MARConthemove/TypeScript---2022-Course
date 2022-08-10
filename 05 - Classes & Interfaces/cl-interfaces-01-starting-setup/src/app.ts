f76dcd9d3b0befd1176cb901e8b5de9f8e2352b3
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-08-10 08:26:34 +0200

    getters and setters with tsc

--- 05 - Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05 - Classes & Interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -41,8 +41,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport
+    }
+    throw new Error("No report found.")
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!")
+    }
+    this.addEmployee(value)
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting")
+    this.lastReport = reports[0]
   }
 
   addEmployee(name: string) {
@@ -55,6 +72,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text)
+    this.lastReport = text
   }
 
   printReports() {
@@ -65,7 +83,15 @@ class AccountingDepartment extends Department {
 const it = new ITDepartment("d1", ["Marc", "Manu"])
 const accounting = new AccountingDepartment("d2", [])
 
+
 accounting.addReport("Something went wrong ...")
+console.log(accounting.mostRecentReport)
+
+accounting.addEmployee("Max")
+accounting.addEmployee("Manu")
+
+accounting.printReports()
+// accounting.printEmployeeInformation()
 
 it.addEmployee("Max")
 it.addEmployee("Manu")
